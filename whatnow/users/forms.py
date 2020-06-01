4cbdd78ee20b51266340f805b8b3390d8028efb2
Yoanna <yoansiyotova@gmail.com>
2020-06-01 21:30:41 +0300

    Change forms

--- whatnow/users/forms.py
+++ whatnow/users/forms.py
@@ -1,17 +1,14 @@
 from django import forms
-# from django.contrib.auth.models import User
-# from django.contrib.auth.forms import UserCreationForm
-# from django import forms
 from .models import Users
 
 
 class UserRegisterForm(forms.ModelForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput, min_length=8)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput, min_length=8)
 
     class Meta:
         model = Users
-        fields = ['username', 'email', 'phone']
+        fields = ['username', 'email', 'phone', 'password']
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
@@ -21,9 +18,8 @@ class UserRegisterForm(forms.ModelForm):
         return email
 
     def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
+        password = self.cleaned_data.get("password")
         password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
+        if password and password2 and password != password2:
             raise forms.ValidationError("Passwords don't match")
-        return password2
+        return password
